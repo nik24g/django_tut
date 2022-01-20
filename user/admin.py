ba44992545e13c7f7774888324f115a9b1a618cc
Nitin <nitingoswami1900@gmail.com>
2022-01-20 22:46:43 +0530

    v2

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,50 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'id', 'username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('email', 'id', 'username', 'first_name', 'last_name', 'is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email',
+                'image',
+                'password',
+                'username',
+                'first_name',
+                'last_name',
+                'address'
+            ),
+        }),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ('wide'),
+            "fields": (
+                'email',
+                'first_name',
+                'last_name',
+                'username',
+                'image',
+                'address',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_active',
+            ),
+        }),
+    )
+
+    ordering = ('email',)
+    
+    
+
+# admin.site.register(User)
 admin.site.register(Contact)
+
--- user/models.py
+++ user/models.py
@@ -1,6 +1,12 @@
+from datetime import date
+from distutils.command.upload import upload
 import email
+from email.headerregistry import Address
+from statistics import mode
 from django.db import models
+from .managers import CustomUserManager
 
+from django.contrib.auth.models import User, AbstractUser, PermissionsMixin
 # Create your models here.
 
 class Contact(models.Model):
@@ -13,3 +19,19 @@ class Contact(models.Model):
     def __str__(self):
         return self.email
     pass
+
+class User(AbstractUser):
+    username = None
+    # Id = models.AutoField
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, default="")
+    address = models.TextField(default="")
+    # date = models.DateField()
+    image = models.ImageField(upload_to='user/image', null=True, blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    object = CustomUserManager()
+
+    def __str__(self):
+        return self.email
